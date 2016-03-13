1462d9b8568a4c8cfe46c5582a493cc3fdd59232
Berend Meijer <berend.sjaak@gmail.com>
2016-03-13 17:29:31 +0100

    - Added new methods for playing and updating the sound source.
    - Added variations of footstep sounds.

--- Confus/Confus/Audio/PlayerAudioEmitter.cpp
+++ Confus/Confus/Audio/PlayerAudioEmitter.cpp
@@ -4,22 +4,25 @@ namespace Confus
 {
     namespace Audio
     {
-        PlayerAudioEmitter::PlayerAudioEmitter(Player a_Player)
+        PlayerAudioEmitter::PlayerAudioEmitter(Player a_AttachedPlayer) : m_AttachedPlayer(&a_AttachedPlayer)
         {
-
+            m_AudioSource = new Confus::OpenALSource("SFX\Player\Footsteps\Concrete\Footstep1_Concrete.wav");
         }
 
         PlayerAudioEmitter::~PlayerAudioEmitter()
         {
+            delete(m_AudioSource);
+            delete(m_AttachedPlayer);
         }
 
-        void PlayerAudioEmitter::PlayFootStepSound()
+        void PlayerAudioEmitter::playFootStepSound()
         {
+            m_AudioSource->play();
         }
 
-        void PlayerAudioEmitter::PlayAttackSound()
+        void PlayerAudioEmitter::updatePosition()
         {
-
+            m_AudioSource->setPosition(m_AttachedPlayer->getPosition());
         }
     }
 }
--- Confus/Confus/Audio/PlayerAudioEmitter.h
+++ Confus/Confus/Audio/PlayerAudioEmitter.h
@@ -1,20 +1,41 @@
 #pragma once
 #include <irrlicht/irrlicht.h>
+#include <memory>
 
 #include "..\Player.h"
+#include "..\OpenAL\OpenALSource.h"
 
 namespace Confus
 {
     namespace Audio
     {
+        /// <summary>
+        /// Class PlayerAudioEmitter emits sounds like footsteps and attacking sounds from each individual player.
+        /// </summary>
         class PlayerAudioEmitter
         {
-        public:
-            PlayerAudioEmitter(Player a_Player);
+            OpenALSource* m_AudioSource;
+            Player* m_AttachedPlayer;
+        public:            
+            /// <summary>
+            /// Initializes a new instance of the <see cref="PlayerAudioEmitter"/> class.
+            /// </summary>
+            /// <param name="a_AttachedPlayer">The player that owns this emitter.</param>
+            PlayerAudioEmitter(Player a_AttachedPlayer);
+            /// <summary>
+            /// Finalizes an instance of the <see cref="PlayerAudioEmitter"/> class.
+            /// </summary>
             ~PlayerAudioEmitter();
 
-            void PlayFootStepSound();
-            void PlayAttackSound();
+            /// <summary>
+            /// Plays a random footstep sound.
+            /// </summary>
+            void playFootStepSound();
+            
+            /// <summary>
+            /// Updates the position of this instance.
+            /// </summary>
+            void updatePosition();
 
         };
     }
