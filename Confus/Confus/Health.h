4ef97209a1edd459c9ba1e2254a7af3f4b6b8e05
NickyW <Nickywild-1@hotmail.com>
2016-03-28 22:33:44 +0200

    Various fixes
    Added constructor, renamed a_Heal to a_Health and removed setHealth

--- Confus/Confus/Health.h
+++ Confus/Confus/Health.h
@@ -9,13 +9,12 @@ namespace Confus
 	private:
 		std::function<bool(irr::scene::ISceneNode* a_DamagedNode)> m_DeathCallback;
 		irr::scene::ISceneNode* m_AttachedNode;
-
+		int m_Health;
 	public:
-		int m_Health = 100;
+		Health();
 		void damage(int a_Damage);
-		void heal(int a_Heal);
+		void heal(int a_Health);
 		void setDeathCallback(const std::function<bool(irr::scene::ISceneNode* a_DamageNode)>& a_DeathCallback);
-		void setHealth(int a_Health);
 		int getHealth();
 	};
 }
