c50d6a88a97e95e43b2dca0315d85d8d51ea0276
Berend Meijer <berend.sjaak@gmail.com>
2016-04-14 12:07:28 +0200

    Fixes the offset of the player camera. Also contains a small player audio bug, forgot to update the position of the sword swoshes with the player.

--- Confus/Confus/Player.cpp
+++ Confus/Confus/Player.cpp
@@ -4,6 +4,7 @@
 #include "EventManager.h"
 #include "Flag.h"
 
+
 namespace Confus
 {
     const irr::u32 Player::WeaponJointIndex = 14u;
@@ -24,7 +25,7 @@ namespace Confus
         PlayerNode = sceneManager->addAnimatedMeshSceneNode(m_Mesh, nullptr);
         PlayerNode->setMaterialFlag(irr::video::E_MATERIAL_FLAG::EMF_LIGHTING, false);
         PlayerNode->setScale(irr::core::vector3df(0.3f, 0.3f, 0.3f));
-        PlayerNode->setPosition(irr::core::vector3df(0.f, -2.f, -0.2f));
+        PlayerNode->setPosition(irr::core::vector3df(0.f, -2.0f, -0.2f));
         PlayerNode->setName({"Player"});
 
         if(a_TeamIdentifier == ETeamIdentifier::TeamBlue) {
@@ -110,7 +111,12 @@ namespace Confus
     void Player::setLevelCollider(irr::scene::ISceneManager* a_SceneManager,
         irr::scene::ITriangleSelector* a_Level)
     {
-        CameraNode->addAnimator(a_SceneManager->createCollisionResponseAnimator(a_Level, PlayerNode, {0.1f, 0.2f, 0.1f}, { 0, -1, 0 }));
+        CameraNode->addAnimator(a_SceneManager->createCollisionResponseAnimator(a_Level, PlayerNode, {0.1f, 0.2f, 0.1f}, { 0, -1, 0 }, {0, 1.5f, 0}));
+        
+        irr::scene::ITriangleSelector* selector = 0;
+        selector = a_SceneManager->createTriangleSelector(PlayerNode);
+        CameraNode->setTriangleSelector(selector);
+        selector->drop();
     }
 
     void Player::startWalking() const
