32ef17c00655ecfda73d4b750200715467fa1fcb
Athos <athosvankralingen@hotmail.com>
2016-04-27 23:45:33 +0200

    Fixed bouncing

--- Confus/ConfusShared/Physics/RigidBody.cpp
+++ Confus/ConfusShared/Physics/RigidBody.cpp
@@ -16,6 +16,7 @@ namespace Confus
 		{
 			m_Body->setMotionState(m_MotionState.get());
 			m_Body->getInvMass() <= static_cast<btScalar>(0.00001) ? makeStatic() : makeDynamic();
+			m_Body->setRestitution(0.0f);
 		}
 
 		void RigidBody::onPrePhysicsUpdate() const
