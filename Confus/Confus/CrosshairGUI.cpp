9ef1444de4b5e197e31b65ef46b1d8db04f465b4
Berend Meijer <berend.sjaak@gmail.com>
2016-05-18 22:01:27 +0200

    Added documentation and removed unneccesary variables.

--- Confus/Confus/CrosshairGUI.cpp
+++ Confus/Confus/CrosshairGUI.cpp
@@ -4,17 +4,16 @@
 
 namespace Confus
 {
-    CrosshairGUI::CrosshairGUI(irr::IrrlichtDevice* a_Device,irr::core::dimension2du a_ImageMaxSize, irr::video::ITexture * a_CrosshairTexture, irr::core::vector2df a_RelativePosition)
-        : m_ImageMaxSize(a_ImageMaxSize)
+    CrosshairGUI::CrosshairGUI(irr::IrrlichtDevice* a_Device,irr::core::dimension2du a_ImageSize, irr::video::ITexture * a_CrosshairTexture, irr::core::vector2df a_RelativePosition)
     {
-        irr::core::dimension2du offset = calculateOffset(m_ImageMaxSize, a_CrosshairTexture);
+        irr::core::dimension2du offset = calculateOffset(a_ImageSize, a_CrosshairTexture);
 
         irr::gui::IGUIEnvironment* guiEnvironment = a_Device->getGUIEnvironment();
         irr::video::IVideoDriver* driver = a_Device->getVideoDriver();
 
 
         m_CrosshairImage = addImage(guiEnvironment, GUI::calculatePixelPosition(a_RelativePosition, a_Device->getVideoDriver()->getScreenSize()) - irr::core::dimension2di(offset.Width / 2, 0),
-            a_CrosshairTexture, irr::video::SColor(255, 255, 255, 255), a_ImageMaxSize);
+            a_CrosshairTexture, irr::video::SColor(255, 255, 255, 255), a_ImageSize);
     }
 
     void CrosshairGUI::update()
--- Confus/Confus/CrosshairGUI.h
+++ Confus/Confus/CrosshairGUI.h
@@ -5,15 +5,30 @@
 
 namespace Confus
 {
+    /// <summary> 
+    /// A class that constructs the crosshair GUI element. 
+    /// Derives from IUIElement.
+    ///       ----------
+    ///      |IUIElement|
+    ///       ----------
+    ///           ^
+    ///           |
+    ///      ------------
+    ///     |CrosshairGUI|
+    ///      ------------
+    /// </summary>
     class CrosshairGUI : public IUIElement
     {
     private:
+        /// <summary> A reference to the crosshair image. </summary>
         irr::gui::IGUIImage* m_CrosshairImage;
-        irr::video::ITexture* m_CrosshairTexture;
-        irr::core::dimension2du m_ImageMaxSize;
-
     public:
-        CrosshairGUI(irr::IrrlichtDevice* a_Device, irr::core::dimension2du a_ImageMaxSize, irr::video::ITexture* a_CrosshairTexture, irr::core::vector2df a_RelativePosition);
+        /// <summary> Crosshair constructor. </summary>
+        /// <param name="a_Device">The active Irrlicht Device.</param>
+        /// <param name="a_ImageSize">The size of the image on the screen.</param>
+        /// <param name="a_CrosshairTexture">The texture to display.</param>
+        /// <param name="a_RelativePosition">The position on the screen, being 0 and 1 at the edges of the screen. </param>
+        CrosshairGUI(irr::IrrlichtDevice* a_Device, irr::core::dimension2du a_ImageSize, irr::video::ITexture* a_CrosshairTexture, irr::core::vector2df a_RelativePosition);
         virtual void update() override;
         ~CrosshairGUI();
     };
