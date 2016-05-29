93158127a973818fca19a33d63bf16eb5323af53
Lansenou <lansenou@live.nl>
2016-05-29 22:58:29 +0200

    Tweaked camera position a little bit.

--- Confus/Confus/CameraController.cpp
+++ Confus/Confus/CameraController.cpp
@@ -12,7 +12,7 @@ namespace Confus
 	{
 		m_Device->getCursorControl()->setPosition(0.5f, 0.5f);
 		m_Device->getCursorControl()->setVisible(false);
-        m_OrbitNode->setPosition(irr::core::vector3df(0.0f, 1.f, 1.5f));
+        m_OrbitNode->setPosition(irr::core::vector3df(0.0f, 0.1f, 2.1f));
 	}
 
 	void CameraController::update()
